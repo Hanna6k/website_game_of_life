4d09a2eea9be77b0bf36f99c5dc73e6a93a4032f
unknown <ha.kradol@ksr.ch>
2023-10-28 21:16:47 +0200

    style.css done

--- app.py
+++ app.py
@@ -1,92 +1,53 @@
 from flask import Flask, render_template, request
-import numpy as np
-from game_of_life_03 import Cell
 from game_of_life_03 import World
-
-
 from game_of_life_03 import pattern_list
-
 
 app = Flask(__name__)
 
-# ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '.', ',', '!', '?']
-# def encryption_caesar(cleartext,n):
-#     encryption = ""
-#     cleartext = cleartext.upper()
-#     for letter in cleartext:       
-#         position = ALPHABET.index(str(letter))
-#         new_position = position + n
-#         if new_position > len(ALPHABET):
-#             new_position -= len(ALPHABET)
-#         new_letter = ALPHABET[new_position]
-#         encryption += new_letter  
-#     return encryption
-
 X_VISUAL_SIZE=800
 Y_VISUAL_SIZE=450
-start_num = (800//15)*(450//15)//5
 CELL_SIZE = 15
+TIME_NEW_CELL = 1000
+number_of_random_cells = (X_VISUAL_SIZE//CELL_SIZE)*(Y_VISUAL_SIZE//CELL_SIZE)//4
 pos_pattern = 0
 
 world = World(random_x_range=X_VISUAL_SIZE, random_y_range=Y_VISUAL_SIZE, cell_size=CELL_SIZE)
 
-
-
-
 @app.route("/")
 def index():
-    return render_template('index.html')
+    canvas_width = X_VISUAL_SIZE
+    canvas_height = Y_VISUAL_SIZE
+    return render_template('index.html',canvas_width=canvas_width, canvas_height=canvas_height)
 
-# @app.route('/encrypt', methods =["POST"])
-# def encrypt():
-#     plain_text = request.json['data']   # gets data from js
-#     cipher_text = encryption_caesar(plain_text, 1)
-#     print("hi")
-#     return{'cipher': cipher_text} #give data to js
-
-@app.route('/cellsize')
+@app.route('/cellsize') # only passes on cell size
 def pass_on_cellsize():
     return{'cellsize': CELL_SIZE} #give data to js
 
-@app.route('/start_cells')
+@app.route('/time_new_update') # only passes on cell size
+def pass_on_time():
+    return{'time_update': TIME_NEW_CELL} #give data to js
+
+@app.route('/random_cells') # passes on coordinates of random generated cells
 def pass_on_startcells():
     world.kill_all_cells()
-    p = world.create_starting_cells(start_num_alive=start_num)
-    # print('ur mum')
-    return p #give data to js
+    random_cells = world.create_random_cells(start_num_alive=number_of_random_cells)
+    return random_cells #give data to js
 
 @app.route('/pattern')
-def pass_on_pattern():
+def pass_on_pattern():  # passes on coordinates of of a pattern
     global pos_pattern
     pos_pattern += 1
     if pos_pattern >= len(pattern_list):
         pos_pattern = 0    
     world.kill_all_cells()
     world.create_pattern(pattern_list[pos_pattern])
-    p = pattern_list[pos_pattern]
-    return p #give data to js
-
+    pattern = pattern_list[pos_pattern]
+    return pattern #give data to js
 
 @app.route('/update')
-def update_cells():
-    p = world.update()
-    # print(p)
-    return p #give data to js
-
-
-# @app.route('/pattern')
-# def pass_on_pattern():
-#     pos_pattern += 1
-#     world.kill_all_cells()
-#     world.create_pattern(pattern_list[pos_pattern]) 
-#     p = pattern_list[pos_pattern]
-#     return p #give data to js
-
-
-# @app.route('/updategame')
-# def pass_on_coords():
-#     coords = list
-#     return coords # dictionary is better not list eventuel umwandeln
+def update_cells(): # passes on the updated coordinates
+    updated_cells = world.update()
+    return updated_cells #give data to js
 
 if __name__ ==  "__main__":
     app.run()
--- static/scripts.js
+++ static/scripts.js
@@ -1,83 +1,84 @@
-console.log("asd");
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
 
-let updateCells = false;
+let updateCells = false; 
 
-async function drawFieldLines(cellSize){
-    const canvasWidth = canvas.width;  // Get the canvas width (e.g., 800)
-    const canvasHeight = canvas.height;  // Get the canvas height (e.g., 450)
+async function drawFieldLines(cellSize){ // this funktion draws all the lines
+    const canvasWidth = canvas.width;  
+    const canvasHeight = canvas.height;  
     const numOfVerticalLines = Math.floor(canvasWidth / cellSize) + 1;
     const numOfHorizontalLines = Math.floor(canvasHeight / cellSize) + 1;
 
     for (let i = 0; i < numOfVerticalLines; i++) {
         pos_x = i*cellSize    
-        context.fillStyle = 'black'; 
+        context.fillStyle ='rgb(51, 51, 51)'; 
         context.fillRect(pos_x, 0, 1,canvasHeight);
     }   
     for (let i = 0; i < numOfHorizontalLines; i++) {
         pos_y = i*cellSize    
-        context.fillStyle = 'black'; 
+        context.fillStyle = 'rgb(51, 51, 51)'; 
         context.fillRect(0, pos_y, canvasWidth,1);
     }  
 }
 
-async function fetchCellSize() {
+async function fetchCellSize() { // gets cell size from app.py
     let response = await fetch('/cellsize', {
-        method: 'GET', // Sending a GET request to retrieve the cell size
+        method: 'GET', 
     });
-
     let jsonData = await response.json();
-    const cellSize = jsonData.cellsize;
-    
+    const cellSize = jsonData.cellsize;    
     return cellSize;
-    // Now you can use the 'cellSize' variable in your JavaScript code
 }
 
-async function patternCells(event){
-    console.log("pressed pattren");
-    const cellSize = await fetchCellSize();
-    console.log(cellSize)
-    let response = await fetch('/pattern',{
-        method: 'GET', // because want to send data to server s.t. it can process it
+
+async function fetchTimeInterval() { // get time_for new uodate but it doesn't work
+    let response = await fetch('/time_new_update', {
+        method: 'GET', 
     });
     let jsonData = await response.json();
-    console.log('fucj');
+    const timeNewCell = jsonData.cellsize;    
+    return timeNewCell;
+}
+
+
+async function patternCells(event){ // gets coordinates of a pattern
+    console.log("pressed pattren");
+    const cellSize = await fetchCellSize();
+    let response = await fetch('/pattern',{
+        method: 'GET', 
+    });
+    let jsonData = await response.json();
     context.clearRect(0, 0, canvas.width, canvas.height);
-    drawFieldLines(cellSize);
-    for (const elemnt of jsonData){
+    for (const elemnt of jsonData){ // draw cells on canvas
         let pos_x = elemnt[0]*cellSize;
         let pos_y = elemnt[1]*cellSize;
-        console.log(elemnt);
-        context.fillStyle = 'blue'; 
+        context.fillStyle = 'rgb(222, 221, 181)'; 
         context.fillRect(pos_x, pos_y, cellSize,cellSize);
-
-    }
-    
+    }   
+    drawFieldLines(cellSize);
 }     
 
 
-async function handleStartCells(event){
-    console.log("pressed start");
+async function randomCells(event){ // gets coordinates of random placed cells
+    console.log("pressed random");
     const cellSize = await fetchCellSize();
 
-    let response = await fetch('/start_cells',{
+    let response = await fetch('/random_cells',{
         method: 'GET', // because want to send data to server s.t. it can process it
     });
     let jsonData = await response.json();
 
     context.clearRect(0, 0, canvas.width, canvas.height);
-    drawFieldLines(cellSize);
-    for (const elemnt of jsonData){
+    for (const elemnt of jsonData){ // draws them
         let pos_x = elemnt[0] * cellSize;
         let pos_y = elemnt[1] * cellSize;
-        console.log(pos_x, pos_y);
-        context.fillStyle = 'blue'; 
+        context.fillStyle = 'rgb(222, 221, 181)'; 
         context.fillRect(pos_x, pos_y, cellSize,cellSize);
     }   
+    drawFieldLines(cellSize);
 }
 
-async function startStop(event){
+async function startStop(event){ // is for the stop and start button
     if (updateCells){
         updateCells = false;
         console.log('stop')
@@ -89,30 +90,24 @@ async function startStop(event){
     }
 }
 
-
-
-
-
-async function cellUpdate(){
+async function cellUpdate(){ // gets the updated coordinates
     if (updateCells){
         const cellSize = await fetchCellSize();
         let response = await fetch('/update',{
-            method: 'GET', // because want to send data to server s.t. it can process it
+            method: 'GET', 
         });
         let jsonData = await response.json();
         context.clearRect(0, 0, canvas.width, canvas.height);
-        drawFieldLines(cellSize);
-        for (const elemnt of jsonData){
+        for (const elemnt of jsonData){// draws cells
             let pos_x = elemnt[0]*cellSize;
             let pos_y = elemnt[1]*cellSize;
-            console.log(elemnt);
-            context.fillStyle = 'blue'; 
+            context.fillStyle = 'rgb(222, 221, 181)'; 
             context.fillRect(pos_x, pos_y, cellSize,cellSize);
         }
+        drawFieldLines(cellSize);
     }
 }
 
+// const time_inbetween_new_update = await fetchTimeInterval();   don't know how i should do this since await only works in async funktions
+setInterval(cellUpdate, 1000); // the cellUpdate funktion is called every second
 
-
-
-setInterval(cellUpdate, 2000);
